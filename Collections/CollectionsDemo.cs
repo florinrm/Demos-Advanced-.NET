2f667b52563bc937de7aea0c36a6f98ad83186e4
Florin Mihalache <florinrmfm3@gmail.com>
2019-09-29 23:09:24 +0300

    Added initial files

---
+++ Collections/CollectionsDemo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Collections
+{
+    public class CollectionsDemo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Multithreading/MultithreadingDemo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multithreading
+{
+    public class MultithreadingDemo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
