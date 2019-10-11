bd949d6fa330dce5f8c03511ee2cc308f6005164
Florin Mihalache <florinrmfm3@gmail.com>
2019-10-11 23:40:16 +0300

    Collections: Added ArrayList example

--- Collections/CollectionsDemo.cs
+++ Collections/CollectionsDemo.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Collections
 {
     public class CollectionsDemo
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // colectii orientate pe obiect
+            // ArrayList contine doar object
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(100);
+            arrayList.Add("Decebal");
+            arrayList.AddRange(new object[] { 1, "fulala", 'c' });
+
+            foreach (var element in arrayList) {
+                Console.WriteLine(element);
+            }
+
+
+            List<int> integersList = new List<int>();
+            integersList.Add(1);
+            integersList.AddRange(new int[] { 2, 3, 4, 5 });
+            Console.WriteLine("Elements from integersList");
+            foreach (var elem in integersList) {
+                Console.WriteLine(elem);
+            }
         }
     }
 }
